3877c6fcd5a265ebd179f3e372644638ac4a604e
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-02-25 13:30:58 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const add = document.getElementById('addition');
+const sub = document.getElementById('subtraction');
+const mult = document.getElementById('multiply');
+const divide = document.getElementById('division');
