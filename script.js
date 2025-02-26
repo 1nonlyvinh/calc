fe83ae3da81f6f143c29f170205846e84e114883
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-02-26 13:32:22 +0000

    init commit

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-const add = document.getElementById('addition');
-const sub = document.getElementById('subtraction');
-const mult = document.getElementById('multiply');
-const divide = document.getElementById('division');
+// a lot of element ids for number values + operations
+
+let opperandOne = "null"; 
+
+const add = document.querySelector(".addition").addEventListener("click", function() {
+    console.log("add");
+});
+const sub = document.querySelector('.subtract').addEventListener("click", function() {
+    console.log("sub");
+});
+const mult = document.querySelector('.multiply').addEventListener("click", function() {
+    console.log("mult");
+});
+const divide = document.querySelector('.division').addEventListener("click", function() {
+    console.log("divide");
+});
+const one = document.querySelector('.one').addEventListener("click", function() {
+    console.log("one");
+});
+const five = document.querySelector('.five').addEventListener("click", function() {
+    console.log("five");
+});
+const three = document.querySelector('.three').addEventListener("click", function() {
+    console.log("three");
+});
+const nine = document.querySelector('.nine').addEventListener("click", function() {
+    console.log("nine");
+});
+const six = document.querySelector('.six').addEventListener("click", function() {
+    console.log("six");
+});
+const two = document.querySelector('.two').addEventListener("click", function() {
+    console.log("two");
+});
+const seven = document.querySelector('.seven').addEventListener("click", function() {
+    console.log("seven");
+});
+const eight = document.querySelector('.eight').addEventListener("click", function() {
+    console.log("eight");
+});
+const zero = document.querySelector('.zero').addEventListener("click", function() {
+    console.log("zero");
+});
+
+function createOpperand() {
+
+}
